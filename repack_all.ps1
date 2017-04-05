b31c4941de38f476365d75fbfc8cb2a8ee1eb6f2
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-05 22:39:49 +0800

    add runas

--- repack_all.ps1
+++ repack_all.ps1
@@ -1,4 +1,11 @@
-﻿WorkFlow Choco-Pack {
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+	Write-Host "获取管理员权限......"
+	Start-Sleep -Seconds 2
+	Start-Process PowerShell "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" " -Verb RunAs;
+	Exit
+}
+
+WorkFlow Choco-Pack {
     $dirs = Get-ChildItem -Directory -Exclude ".git"
     foreach -parallel ($dir in $dirs) {
         $null = choco pack $(Join-Path $dir "$($dir.Name).nuspec") --out "$dir"
