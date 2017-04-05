f70272ed9cbd71a4db9fcad9086ccf3a06e21c18
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-05 22:53:01 +0800

    update

--- update_all.ps1
+++ update_all.ps1
@@ -6,6 +6,8 @@ if (Test-Path $PSScriptRoot/update_vars.ps1) {
     . $PSScriptRoot/update_vars.ps1
 }
 
+$reportPath = Join-Path $PSScriptRoot "README.md"
+
 $Options = [ordered]@{
     Timeout = 60
     UpdateTimeout = 600
@@ -14,7 +16,7 @@ $Options = [ordered]@{
 
     Report = @{
         Type = 'markdown'
-        Path = "$PSScriptRoot\README.md"
+        Path = "$reportPath.tmp"
         Params = @{
             NoAppVeyor = $false
             NoIcons = $false
@@ -47,9 +49,12 @@ if ($ForcedPackages) {
 $global:au_Root = $Root
 $global:info = Update-AUPackages -Name $Name -Options $Options
 
-if ($global:info.updated) {
+if (($global:info | Where-Object Updated -eq $true | Measure-Object).Count -gt 0) {
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+    [System.IO.File]::WriteAllLines($reportPath, $(Get-Content "$reportPath.tmp"), $Utf8NoBomEncoding)
+
     git add "README.md"
-    git comment -m "AU: $($packages.Length) updated"
+    git commit -m "AU: $($packages.Length) updated"
 
     Write-Host "Pushing Report"
     git push -q
