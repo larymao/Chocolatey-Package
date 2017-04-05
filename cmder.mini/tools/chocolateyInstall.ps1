c5447e0403e94c8bcf75b1227a2d5131ecc96d44
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-05 22:29:29 +0800

    update cmder.mini

--- cmder.mini/tools/chocolateyInstall.ps1
+++ cmder.mini/tools/chocolateyInstall.ps1
@@ -2,11 +2,15 @@
 
 $packageName  = 'cmder'
 $url          = 'https://github.com/cmderdev/cmder/releases/download/v1.3.2/cmder_mini.zip'
+$checksum     = '3AF9F74A162103251111B9A458315007'
+$checksumType = 'md5'
 $toolsPath    = Join-Path $(Get-ToolsLocation) $packageName
 
 $packageArgs = @{
     packageName   = $packageName
     url           = $url
+    checksum      = $checksum
+    checksumType  = $checksumType
     unzipLocation = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
--- cmder.mini/update.ps1
+++ cmder.mini/update.ps1
@@ -13,10 +13,15 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $request = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/cmderdev/cmder/releases/latest" -MaximumRedirection 0 -ErrorAction Ignore
     $version = ($request.Headers.Location -split "/" -match "^v\d+(\.\d+)+").Replace("v", '').Trim()
+
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/cmderdev/cmder/releases/download/v$version/hashes.txt"
+    $md5sum = ($page.RawContent -split "\n" | Select-Object -Last 1 -Skip 1).Trim()
 	
     return @{
-        Version = $version;
-        URL32 = "https://github.com/cmderdev/cmder/releases/download/v${version}/cmder_mini.zip";
+        Version = $version
+        URL32 = "https://github.com/cmderdev/cmder/releases/download/v${version}/cmder_mini.zip"
+        Checksum32 = $md5sum
+        ChecksumType32 = 'md5'
     }
 }
 
