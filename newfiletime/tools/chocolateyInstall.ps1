97726689c0d12158416c087147cd682331a7db30
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-06 14:30:00 +0800

    update newfiletime

--- newfiletime/tools/chocolateyInstall.ps1
+++ newfiletime/tools/chocolateyInstall.ps1
@@ -1,15 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName    = 'newfiletime'
-$url32          = 'http://www.softwareok.com/Download/NewFileTime.zip'
-$url64          = 'http://www.softwareok.com/Download/NewFileTime_x64.zip'
+$url32          = ''
+$checksum32     = ''
+$checksumType32 = ''
+$url64          = ''
+$checksum64     = ''
+$checksumType64 = ''
 $toolsPath      = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
 	packageName    = $packageName
 	url            = $url32
+	checksum       = $checksum32
+	checksumType   = $checksumType32
 	url64          = $url64
+	checksum64     = $checksum64
+	checksumType64 = $checksumType64
 	unzipLocation  = $toolsPath
 }
-
 Install-ChocolateyZipPackage @packageArgs
--- newfiletime/update.ps1
+++ newfiletime/update.ps1
@@ -3,6 +3,12 @@
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^[$]url64\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType32\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(^[$]checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
         }
     }
 }
@@ -17,4 +23,4 @@ function global:au_GetLatest {
     }
 }
 
-Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
+Update-Package -NoCheckUrl -NoCheckChocoVersion
