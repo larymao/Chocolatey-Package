f12122c055cdf7b0e2ed84c245c8bc04a9e6e936
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-06 14:13:56 +0800

    add nodejs.lts

---
+++ nodejs.lts/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'nodejs'
+$url32 = 'https://npm.taobao.org/mirrors/node/v6.10.2/node-v6.10.2-win-x86.7z'
+$checksum32 = '4149533ef54039cef1e49eb5e41cbb1688e5509751a4f518b05d330e38dc5eea'
+$checksumType32 = 'sha256'
+$url64 = 'https://npm.taobao.org/mirrors/node/v6.10.2/node-v6.10.2-win-x64.7z'
+$checksum64 = '08572d6714ab8e76cfac6211b63fcf2062ff443355be28a0f106d2cc05c45e46'
+$checksumType64 = 'sha256'
+$toolsPath = $(Get-ToolsLocation)
+
+$packageArgs = @{
+    packageName = $packageName
+    url = $url32
+    checksum = $checksum32
+    checksumType = $checksumType32
+    url64 = $url64
+    checksum64 = $checksum64
+    checksumType64 = $checksumType64
+    unzipLocation = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$unzipPath = (Get-ChildItem $toolsPath -Directory | Where-Object Name -Like "node-*-win-x*" | Select-Object -First 1).FullName
+$nodejsPath = Join-Path $toolsPath "nodejs"
+Move-Item -Path $unzipPath -Destination $nodejsPath -Force
+Install-ChocolateyPath -PathToInstall $nodejsPath -PathType 'Machine'
---
+++ nodejs.lts/tools/chocolateyUninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'nodejs'
+$toolsPath = Join-Path $(Get-ToolsLocation) $packageName
+
+$envPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
+if (!$envPath.ToLower().Contains($toolsPath.ToLower())) {
+    Write-Host "PATH environment variable does not have $toolsPath in it."
+    return
+}
+
+$envPath = $envPath -split ";"
+$envPath = $envPath -ne $toolsPath
+[Environment]::SetEnvironmentVariable('Path', $envPath, [System.EnvironmentVariableTarget]::Machine)
+Remove-Item $toolsPath -Recurse -Force
