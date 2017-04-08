fe1082196d3b70ad398caff4f520997c0092ab35
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-08 11:41:03 +0800

    AU: 2 updated - filevista golang [skip ci]

--- golang/tools/chocolateyInstall.ps1
+++ golang/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName    = 'golang'
-$url32          = 'https://storage.googleapis.com/golang/go1.8.windows-386.zip'
-$checksum32     = 'b8199a4af9327807803d2892268dfbffb615f98d717cd824833a97e77f981f46'
+$url32          = 'https://storage.googleapis.com/golang/go1.8.1.windows-386.zip'
+$checksum32     = '9738365e64d4d80ea54487915c9bfc0c3bbaa7d68ec78487988f80ebe8129c57'
 $checksumType32 = 'SHA256'
-$url64          = 'https://storage.googleapis.com/golang/go1.8.windows-amd64.zip'
-$checksum64     = 'cb27fe210f3a9d10329d48514895d2a1e3651125a7c3c758f0358a5bfc0e3060'
+$url64          = 'https://storage.googleapis.com/golang/go1.8.1.windows-amd64.zip'
+$checksum64     = 'bb6f0fbef8b80c382455af8699bfbb7fe89256d4baf06d927feaeceb7342e4ee'
 $checksumType64 = 'SHA256'
 $toolsPath      = $env:ChocolateyToolsLocation
 
