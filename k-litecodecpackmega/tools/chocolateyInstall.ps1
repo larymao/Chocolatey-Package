d17e0169810e5a0f1114377d1bec311e464a96ee
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-15 08:00:53 +0800

    AU: 3 updated - database.net.free k-litecodecpackmega x64dbg.portable [skip ci]

--- k-litecodecpackmega/tools/chocolateyInstall.ps1
+++ k-litecodecpackmega/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName  = 'k-litecodecpackmega'
-$url          = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1300_Mega.exe'
-$checksum     = '49c08d091c4ce703df5200afa326a7d6f35977dc4ff72a5c9265ddb6e848897a'
+$url          = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1310_Mega.exe'
+$checksum     = 'e1e71ab87aac18976b41b3aef2bfa35f4d45550b937e9a5a03bd208ccfb29eac'
 $checksumType = 'sha256'
 
 $packageArgs = @{
