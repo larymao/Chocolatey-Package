ad9a3bbba57d1aec5f7ea02c10aff78a4394647a
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-15 15:22:13 +0800

    add openssl

---
+++ openssl/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openssl'
+$url32 = 'https://slproweb.com//download/Win32OpenSSL_Light-1_1_0e.exe'
+$url64 = 'https://slproweb.com//download/Win32OpenSSL_Light-1_1_0e.exe'
+$toolsPath = Join-Path $(Get-ToolsLocation) $packageName
+
+$packageArgs = @{
+    packageName = $packageName
+    #InnoSetup - http://unattended.sourceforge.net/InnoSetup_Switches_ExitCodes.html
+    silentArgs = '/silent', '/verysilent', '/sp-', '/suppressmsgboxes',
+    "/DIR=`"$toolsPath`"";
+    url = $url32
+    url64bit = $url64
+}
+Install-ChocolateyPackage @packageArgs
