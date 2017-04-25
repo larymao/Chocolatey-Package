bf0a52056db40c64018e82b5dd0a487074433267
Echizen Ryoma <Echizen Ryoma>
2017-04-25 08:10:27 +0800

    AU: 2 updated - filevista x64dbg.portable [skip ci]

--- x64dbg.portable/tools/chocolateyInstall.ps1
+++ x64dbg.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'x64dbg.portable'
-$url = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2017-04-22_05-05.zip/download'
+$url = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2017-04-24_02-48.zip/download'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
