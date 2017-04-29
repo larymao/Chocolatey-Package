09dfbc8be74cb4f87bda0268aec22fd1a8e2856c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-04-29 10:27:30 +0800

    add sourcetree.portable

---
+++ sourcetree.portable/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'sourcetree.portable'
+$url = 'https://downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-2.0.19.1.exe'
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+    packageName = $packageName
+    url = $url
+    unzipLocation = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$filePath = Get-Item $(Join-Path $toolsPath "SourceTree*.nupkg")
+$packageArgs = @{
+    packageName = $packageName
+    url = $filePath
+    unzipLocation = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+Get-Item $(Join-Path $toolsPath "*") -Exclude "*.ps1", "lib" | Remove-Item -Force -Recurse
+$files = Join-Path $(Get-Item $(Join-Path $toolsPath "lib\*")) "*"
+Move-Item $files $toolsPath -Force
+Remove-Item $(Join-Path $toolsPath "lib") -Force -Recurse
+
+Get-ChildItem $toolsPath -Include "*.exe" -Exclude "SourceTree.exe" -File -Recurse | ForEach-Object { New-Item "$($_.FullName).ignore" -Type file -Force | Out-Null }
---
+++ sourcetree.portable/update.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://www.sourcetreeapp.com/"
+    $url = ($page.Links | Where-Object href -Match "SourceTreeSetup" | Select-Object -First 1 -ExpandProperty href).Trim()
+    $version = $url -split "-|\.exe" -match "\d+(\.\d+){2,4}"
+	
+    return @{
+        Version = $version
+        URL32 = $url
+    }
+}
+
+Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
