caf198a35225135dc55b810f2c14a29a96758505
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-05-02 11:15:01 +0800

    use UseBasicParsing

--- pkhex/update.ps1
+++ pkhex/update.ps1
@@ -8,8 +8,9 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -Uri "https://projectpokemon.org/forums/files/file/1-pkhex/"
-    $version = ($page.Links | Where-Object outerHTML -Match "\d+(\.\d+){1,4}" | Select-Object -First 1 -ExpandProperty innerText).Trim()
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://projectpokemon.org/forums/files/file/1-pkhex/"
+    $version = ($page.Links | Where-Object outerHTML -Match "\d+(\.\d+){1,4}" | Select-Object -First 1 -ExpandProperty outerHTML).Trim()
+    $version = $version -split "<|>" -match "\d+(\.\d+){1,4}"
 	
     return @{
         Version = $version;
