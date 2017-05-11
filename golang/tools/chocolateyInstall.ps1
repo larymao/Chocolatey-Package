212e8028a57240ff9bede66ad469e497afee4d3e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-05-12 07:27:26 +0800

    update golang

--- golang/tools/chocolateyInstall.ps1
+++ golang/tools/chocolateyInstall.ps1
@@ -1,29 +1,29 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageName    = 'golang'
-$url32          = 'https://storage.googleapis.com/golang/go1.8.1.windows-386.zip'
-$checksum32     = '9738365e64d4d80ea54487915c9bfc0c3bbaa7d68ec78487988f80ebe8129c57'
+$packageName = 'golang'
+$url32 = 'https://storage.googleapis.com/golang/go1.8.1.windows-386.zip'
+$checksum32 = '9738365e64d4d80ea54487915c9bfc0c3bbaa7d68ec78487988f80ebe8129c57'
 $checksumType32 = 'SHA256'
-$url64          = 'https://storage.googleapis.com/golang/go1.8.1.windows-amd64.zip'
-$checksum64     = 'bb6f0fbef8b80c382455af8699bfbb7fe89256d4baf06d927feaeceb7342e4ee'
+$url64 = 'https://storage.googleapis.com/golang/go1.8.1.windows-amd64.zip'
+$checksum64 = 'bb6f0fbef8b80c382455af8699bfbb7fe89256d4baf06d927feaeceb7342e4ee'
 $checksumType64 = 'SHA256'
-$toolsPath      = $env:ChocolateyToolsLocation
+$installationPath = Get-ToolsLocation
 
 $packageArgs = @{
-	packageName    = $packageName
-	url            = $url32
-	checksum       = $checksum32
-	checksumType   = $checksumType32
-	url64          = $url64
-	checksum64     = $checksum64
-	checksumType64 = $checksumType64
-	unzipLocation  = $toolsPath
+    packageName    = $packageName
+    url            = $url32
+    checksum       = $checksum32
+    checksumType   = $checksumType32
+    url64          = $url64
+    checksum64     = $checksum64
+    checksumType64 = $checksumType64
+    unzipLocation  = $installationPath
 }
 Install-ChocolateyZipPackage @packageArgs
 
-$goroot = Join-Path "$toolsPath" 'go'
-$gopath = Join-Path "$goroot" 'opt'
-$gorootBin = Join-Path "$goroot" 'bin'
-Install-ChocolateyEnvironmentVariable -VariableName 'GOROOT' -VariableValue "$goroot" -VariableType 'Machine'
-Install-ChocolateyEnvironmentVariable -VariableName 'GOPATH' -VariableValue "$gopath" -VariableType 'Machine'
-Install-ChocolateyPath -PathToInstall $gorootBin -PathType 'Machine'
+$GOROOT = Join-Path $installationPath 'go'
+$GOPATH = Join-Path $GOROOT 'opt'
+$GOBIN = Join-Path $GOROOT 'bin'
+Install-ChocolateyEnvironmentVariable -VariableName 'GOROOT' -VariableValue $GOROOT -VariableType 'Machine'
+Install-ChocolateyEnvironmentVariable -VariableName 'GOPATH' -VariableValue $GOPATH -VariableType 'Machine'
+Install-ChocolateyPath -PathToInstall $GOBIN -PathType 'Machine'
--- golang/tools/chocolateyUninstall.ps1
+++ golang/tools/chocolateyUninstall.ps1
@@ -1,7 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$installationPath = Join-Path $env:ChocolateyToolsLocation "go"
+Remove-Item $installationPath -Recurse -Force
+
 Uninstall-ChocolateyEnvironmentVariable -VariableName 'GOROOT' -VariableType 'Machine'
 Uninstall-ChocolateyEnvironmentVariable -VariableName 'GOPATH' -VariableType 'Machine'
 
-$toolsPath = Join-Path $env:ChocolateyToolsLocation "go"
-Remove-Item $toolsPath -Recurse -Force
+$envPath = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) -split ';' -notmatch '\\go\\'
+[Environment]::SetEnvironmentVariable('Path', $envPath -join ';', [EnvironmentVariableTarget]::Machine)
