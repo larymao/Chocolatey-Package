600dadc19588a8792668dfb9fa4ce29a2174d9d8
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-05-13 20:55:26 +0800

    update

--- sourcetree.portable/tools/chocolateyInstall.ps1
+++ sourcetree.portable/tools/chocolateyInstall.ps1
@@ -2,26 +2,31 @@
 
 $packageName = 'sourcetree.portable'
 $url = 'https://downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-2.0.20.1.exe'
+$checksum = 'df4fb4abf3385f3a73ec47d83689af73f42367d01ff0afd394b50c1190e01d01'
+$checksumType = 'sha256'
 $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-    packageName = $packageName
-    url = $url
+    packageName   = $packageName
+    url           = $url
     unzipLocation = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
 
 $filePath = Get-Item $(Join-Path $toolsPath "SourceTree*.nupkg")
 $packageArgs = @{
-    packageName = $packageName
-    url = $filePath
+    packageName   = $packageName
+    url           = $filePath
     unzipLocation = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
 
 Get-Item $(Join-Path $toolsPath "*") -Exclude "*.ps1", "lib" | Remove-Item -Force -Recurse
-$files = Join-Path $(Get-Item $(Join-Path $toolsPath "lib\*")) "*"
+$lib = Join-Path $toolsPath "lib"
+$files = Join-Path $(Get-Item $(Join-Path $lib "net*")) "*"
 Move-Item $files $toolsPath -Force
-Remove-Item $(Join-Path $toolsPath "lib") -Force -Recurse
+if (Test-Path $lib) {
+    Remove-Item $lib -Force -Recurse
+}
 
 Get-ChildItem $toolsPath -Include "*.exe" -Exclude "SourceTree.exe" -File -Recurse | ForEach-Object { New-Item "$($_.FullName).ignore" -Type file -Force | Out-Null }
--- sourcetree.portable/update.ps1
+++ sourcetree.portable/update.ps1
@@ -3,19 +3,27 @@
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+            "(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
         }
     }
 }
 
 function global:au_GetLatest {
-    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://www.sourcetreeapp.com/"
-    $url = ($page.Links | Where-Object href -Match "SourceTreeSetup" | Select-Object -First 1 -ExpandProperty href).Trim()
-    $version = $url -split "-|\.exe" -match "\d+(\.\d+){2,4}"
+    $page = Invoke-WebRequest -UseBasicParsing -Uri "https://www.sourcetreeapp.com/update/windows/ga/RELEASES"
+    $page = [System.Text.Encoding]::ASCII.GetString($page.Content)
+
+    $release = ($page -split "\n" -notmatch "delta"  | ConvertFrom-String -PropertyNames md5sum, filename, filesize) | Select-Object -Last 1
+
+    $url = "https://www.sourcetreeapp.com/update/windows/ga/" + $release.filename
+    $version = $release.filename -split "-|\.exe" -match "^\d+(\.\d+){2,}$"
 	
     return @{
-        Version = $version
-        URL32 = $url
+        Version        = $version
+        URL32          = $url
+        Checksum32     = ($release.md5sum).ToLower()
+        ChecksumType32 = 'md5'
     }
 }
 
