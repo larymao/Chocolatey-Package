09665572ba69361a0cc148ee0fed7639e215f928
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-05-13 20:58:54 +0800

    change checksum

--- sourcetree.portable/update.ps1
+++ sourcetree.portable/update.ps1
@@ -14,7 +14,7 @@ function global:au_GetLatest {
     $page = Invoke-WebRequest -UseBasicParsing -Uri "https://www.sourcetreeapp.com/update/windows/ga/RELEASES"
     $page = [System.Text.Encoding]::ASCII.GetString($page.Content)
 
-    $release = ($page -split "\n" -notmatch "delta"  | ConvertFrom-String -PropertyNames md5sum, filename, filesize) | Select-Object -Last 1
+    $release = ($page -split "\n" -notmatch "delta"  | ConvertFrom-String -PropertyNames sha1sum, filename, filesize) | Select-Object -Last 1
 
     $url = "https://www.sourcetreeapp.com/update/windows/ga/" + $release.filename
     $version = $release.filename -split "-|\.exe" -match "^\d+(\.\d+){2,}$"
@@ -22,8 +22,8 @@ function global:au_GetLatest {
     return @{
         Version        = $version
         URL32          = $url
-        Checksum32     = ($release.md5sum).ToLower()
-        ChecksumType32 = 'md5'
+        Checksum32     = ($release.sha1sum).ToLower()
+        ChecksumType32 = 'sha1'
     }
 }
 
