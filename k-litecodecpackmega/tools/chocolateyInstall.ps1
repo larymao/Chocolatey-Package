cb700f59614d1cf1b05e96846d9d70815f3a8f85
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-05-25 08:01:17 +0800

    AU: 3 updated - golang k-litecodecpackmega x64dbg.portable [skip ci]

--- k-litecodecpackmega/tools/chocolateyInstall.ps1
+++ k-litecodecpackmega/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName  = 'k-litecodecpackmega'
-$url          = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1316_Mega.exe'
-$checksum     = 'b7a8e0009b42fb265a0b7f642a087b57f562671b4549019df0e05bc5da094d40'
+$url          = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1320_Mega.exe'
+$checksum     = '1c5e021bce8e2905c6658a387614b57291bb5508cc9b048731ec188e9e3e76d1'
 $checksumType = 'sha256'
 
 $packageArgs = @{
