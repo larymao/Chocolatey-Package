bb86a9be18ad011452dfd762905ca1bb7dcd7c5e
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-05-28 11:40:12 +0800

    add openssl

---
+++ openssl/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'openssl'
+$installationPath = Get-ToolsLocation
+$OpenSSL_HOME = Join-Path $installationPath $packageName
+if (Test-Path $OpenSSL_HOME) {
+    Remove-Item -Path $OpenSSL_HOME -Recurse -Force
+}
