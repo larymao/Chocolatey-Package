83c5507f6214e78008593d15b938e303aa6c3fde
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-06-09 20:45:12 +0800

    change unzip mod

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -1,14 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'vscode.portable'
-$url = 'https://vscode.cdn.azure.cn/stable/376c52b955428d205459bea6619fc161fc8faacf/VSCode-win32-ia32-1.13.0.zip'
-$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$PackageName = 'vscode.portable'
+$Url = 'https://vscode.cdn.azure.cn/stable/376c52b955428d205459bea6619fc161fc8faacf/VSCode-win32-ia32-1.13.0.zip'
+$ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
-$packageArgs = @{
-    packageName = $packageName
-    url = $url
-    unzipLocation = $toolsPath
+$PackageArgs = @{
+    PackageName   = $PackageName
+    Url           = $Url
+    UnzipLocation = $ToolsPath
 }
+Install-ChocolateyZipPackage @PackageArgs
 
-Install-ChocolateyZipPackage @packageArgs
-Get-ChildItem $toolsPath -Include "*.exe" -Exclude "Code.exe" -Recurse | ForEach-Object { New-Item "$($_.FullName).ignore" -Type file -Force | Out-Null }
+$FileName = [IO.Path]::GetFileNameWithoutExtension($Url)
+$UnzipPath = $FileName.SubString(0, $FileName.LastIndexOf('-'))
+$UnzipPath = Join-Path $ToolsPath $UnzipPath 
+Move-Item -Path $UnzipPath -Destination $ToolsPath -Force
+Remove-Item -Path $UnzipPath -Recurse -Force
+Get-ChildItem $ToolsPath -Include "*.exe" -Exclude "Code.exe" -Recurse | ForEach-Object { New-Item "$($_.FullName).ignore" -Type file -Force | Out-Null }
--- vscode.portable/update.ps1
+++ vscode.portable/update.ps1
@@ -1,23 +1,23 @@
 ﻿Import-Module AU
 
 function global:au_SearchReplace {
-	@{
-		'tools\chocolateyInstall.ps1' = @{
-			"(^[$]url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-		}
-	}
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        }
+    }
 }
 
 function global:au_GetLatest {
-	$url = "https://vscode-update.azurewebsites.net/latest/win32-archive/stable"
-	$request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
-	$url = $request.Headers.Location
-	$version = $url.Substring($url.LastIndexOf("-") + 1).Replace(".zip", "").Trim()
+    $url = "https://vscode-update.azurewebsites.net/latest/win32-archive/stable"
+    $request = Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore
+    $url = $request.Headers.Location
+    $version = $url.Substring($url.LastIndexOf("-") + 1).Replace(".zip", "").Trim()
 
-	return @{
-		Version = $version
-		URL32 = $url
-	}
+    return @{
+        Version = $version
+        URL32   = $url
+    }
 }
 
 Update-Package -NoCheckUrl -NoCheckChocoVersion -ChecksumFor none
