bea557123e6774dac619db8c37132dca3482623b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-06-11 08:01:19 +0800

    AU: 1 updated - x64dbg.portable [skip ci]

--- x64dbg.portable/tools/chocolateyInstall.ps1
+++ x64dbg.portable/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'x64dbg.portable'
-$url = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2017-06-07_04-37.zip/download'
+$url = 'https://sourceforge.net/projects/x64dbg/files/snapshots/snapshot_2017-06-08_22-37.zip/download'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
