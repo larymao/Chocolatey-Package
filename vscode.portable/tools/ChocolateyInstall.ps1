3a6642812bc7cc80ee4b08d9d286ce959df5cfb3
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-06-17 09:20:06 +0800

    fix bug

--- vscode.portable/tools/ChocolateyInstall.ps1
+++ vscode.portable/tools/ChocolateyInstall.ps1
@@ -14,6 +14,6 @@ Install-ChocolateyZipPackage @PackageArgs
 $FileName = [IO.Path]::GetFileNameWithoutExtension($Url)
 $UnzipPath = $FileName.SubString(0, $FileName.LastIndexOf('-'))
 $UnzipPath = Join-Path $ToolsPath $UnzipPath 
-Move-Item -Path $UnzipPath -Destination $ToolsPath -Force
+Move-Item -Path $(Join-Path $UnzipPath "*") -Destination $ToolsPath -Force
 Remove-Item -Path $UnzipPath -Recurse -Force
 Get-ChildItem $ToolsPath -Include "*.exe" -Exclude "Code.exe" -Recurse | ForEach-Object { New-Item "$($_.FullName).ignore" -Type file -Force | Out-Null }
