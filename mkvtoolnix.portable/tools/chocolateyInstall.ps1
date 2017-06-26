a92a794606338f0de5a235ee56cdee9db88f50a5
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-06-26 09:38:43 +0800

    AU: 1 updated - mkvtoolnix.portable [skip ci]

--- mkvtoolnix.portable/tools/chocolateyInstall.ps1
+++ mkvtoolnix.portable/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName    = 'mkvtoolnix.portable'
-$url32          = 'https://mkvtoolnix.download/windows/releases/12.0.0/mkvtoolnix-32bit-12.0.0.7z'
-$url64          = 'https://mkvtoolnix.download/windows/releases/12.0.0/mkvtoolnix-64bit-12.0.0.7z'
-$checksum32     = '4c9327b01ed46cfc75a6e80dacb2eba6ebf521b4'
+$url32          = 'https://mkvtoolnix.download/windows/releases/13.0.0/mkvtoolnix-32bit-13.0.0.7z'
+$url64          = 'https://mkvtoolnix.download/windows/releases/13.0.0/mkvtoolnix-64bit-13.0.0.7z'
+$checksum32     = '2fc70635ed248cf77e43295059eae94edca16678'
 $checksumType32 = 'sha1'
-$checksum64     = '64bf269080078b54a57cf1a5514124df19228a13'
+$checksum64     = '747cdbb742861bce421a5b6660c8a4d8b71b04d9'
 $checksumType64 = 'sha1'
 $toolsPath      = Split-Path -parent $MyInvocation.MyCommand.Definition
 
