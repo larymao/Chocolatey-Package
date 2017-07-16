c10f94a422e991b857957be047402982615ae15b
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-07-16 10:22:31 +0800

    update

---
+++ update_all.ps1
@@ -0,0 +1,8 @@
+﻿$dirs = Get-ChildItem -Directory -Exclude ".git"
+foreach ($dir in $dirs) {
+    Set-Location $dir
+    $ps1 = Join-Path $dir "update_all.ps1"
+    if (Test-Path $ps1) {
+        & $ps1
+    }
+}
