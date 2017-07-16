e79153873b99407c669545b8a85e2493c41e4069
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2017-07-16 10:02:12 +0800

    change

--- Private/update_all.ps1
+++ Private/update_all.ps1
@@ -6,23 +6,11 @@ if (Test-Path $PSScriptRoot/update_vars.ps1) {
     . $PSScriptRoot/update_vars.ps1
 }
 
-$reportFile = Join-Path $PSScriptRoot "README.md"
-
 $Options = [ordered]@{
     Timeout = 60
     UpdateTimeout = 600
     Threads = 10
-    Push = $Env:au_Push -eq 'true'
-
-    Report = @{
-        Type = 'markdown'
-        Path = "$reportFile.tmp"
-        Params = @{
-            NoAppVeyor = $false
-            NoIcons = $false
-            IconSize = 32
-        }
-    }
+    Push = $false
 
     Git = @{
         User = $Env:github_user
@@ -48,15 +36,3 @@ if ($ForcedPackages) {
 }
 $global:au_Root = $Root
 $global:info = Update-AUPackages -Name $Name -Options $Options
-
-if (($global:info | Where-Object Updated -eq $true | Measure-Object).Count -gt 0) {
-    Get-Content -Path "$reportFile.tmp" | Set-Content -Path $reportFile -Encoding UTF8
-
-    git add "README.md"
-    git commit -m "report update"
-
-    Write-Host "Pushing Report"
-    git push -q
-
-    Remove-Item -Path "$reportFile.tmp" -Force | Out-Null
-}
--- repack_all.ps1
+++ repack_all.ps1
@@ -1,7 +1,10 @@
 ﻿WorkFlow Choco-Pack {
-    $dirs = Get-ChildItem -Directory -Exclude ".git"
+    $dirs = Get-ChildItem -Directory -Exclude ".git" -Recurse
     foreach -parallel ($dir in $dirs) {
-        $null = choco pack $(Join-Path $dir "$($dir.Name).nuspec") --out "$dir"
+        $nuspec = Get-ChildItem -File $(Join-Path $dir "*.nuspec")
+        if (($nuspec | Measure-Object).Count -ge 1) {
+            $null = choco pack $(Join-Path $dir "$($dir.Name).nuspec") --out "$dir"
+        }       
     }
 }
 
